883564ce267655a28695d75cc22d762ea48a0fbc
Anton Kriese <anton.kriese@gmx.de>
2021-05-15 11:52:18 +0200

    add sine playback with variable frequency and duration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::string::String;
-use rodio::{Decoder, OutputStream, source::Source};
+use std::time::Duration;
+use rodio::{Decoder, OutputStream, Sink, source::{Source, SineWave}};
 use std::env;
 
+
+fn play_sine(frequency: u32, seconds: f32) {
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    let source = SineWave::new(frequency).take_duration(Duration::from_secs_f32(seconds));
+    sink.append(source);
+    sink.sleep_until_end();
+}
 
 fn play_mp3(filename: &String) {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
