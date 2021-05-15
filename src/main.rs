fc95f4c3a5a5ba985cb42fcdf4199b4c6a6d2686
Anton Kriese <anton.kriese@gmx.de>
2021-05-15 11:21:34 +0200

    play mp3 file for 5 seconds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::string::String;
+use rodio::{Decoder, OutputStream, source::Source};
+use std::env;
+
+
+fn play_mp3(filename: &String) {
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let file = BufReader::new(File::open(filename).unwrap());
+    let source = Decoder::new(file).unwrap();
+    stream_handle.play_raw(source.convert_samples());
+    std::thread::sleep(std::time::Duration::from_secs(5));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let sound_file_name = &args[1];
+    play_mp3(sound_file_name);
+    println!("The song is over!");
 }
