3a9c5a9bdd1679e7511de24538e13c3074ac3a3b
Anton Kriese <anton.kriese@gmx.de>
2021-05-24 13:13:11 +0200

    setup jackaudio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[cfg(target_os = "windows")]
+#[link(name = "C:/Program Files/JACK2/lib/libjack64")]
+extern "C" {}
+//use jack::Client
+
 fn main() {
-    println!("Hello, world!");
+    let (client, status) =
+        jack::Client::new("rust_jack_sine", jack::ClientOptions::NO_START_SERVER).unwrap();
+    println!("{}'s status is {:?}'", client.name(), status);
+
+    let in_port = client.register_port("audio_in", jack::AudioIn);
+    let out_port = client.register_port("audio_out", jack::AudioOut);
+    jack::
 }
