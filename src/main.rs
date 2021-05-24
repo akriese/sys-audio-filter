6ba5350b9c35c0fb897fc27a185120ab3440ed1c
Anton Kriese <anton.kriese@gmx.de>
2021-05-24 11:43:12 +0200

    play silence, obviously nothing happens

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate cpal;
 extern crate anyhow;
 use cpal::{Sample, SampleFormat};
-use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 fn write_silence<T: Sample>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
     for sample in data.iter_mut() {
@@ -29,20 +29,20 @@ fn enum_devices() -> Result<(), anyhow::Error> {
 
 fn main() {
     enum_devices().expect("Well, something went wrong apparently...");
-    //enum_devices(host.devices());
-    //let device = host.default_output_device().expect("no output device available");
-    //let mut supported_configs_range = device.supported_output_configs()
-        //.expect("error while querying configs");
-    //let supported_config = supported_configs_range.next()
-        //.expect("no supported config?!")
-        //.with_max_sample_rate();
-    //let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
-    //let sample_format = supported_config.sample_format();
-    //let config = supported_config.into();
-    //let stream = match sample_format {
-        //SampleFormat::F32 => device.build_output_stream(&config, write_silence::<f32>, err_fn),
-        //SampleFormat::I16 => device.build_output_stream(&config, write_silence::<i16>, err_fn),
-        //SampleFormat::U16 => device.build_output_stream(&config, write_silence::<u16>, err_fn),
-    //}.unwrap();
-    //stream.play().unwrap();
+    let host = cpal::default_host();
+    let device = host.default_output_device().expect("no output device available");
+    let mut supported_configs_range = device.supported_output_configs()
+        .expect("error while querying configs");
+    let supported_config = supported_configs_range.next()
+        .expect("no supported config?!")
+        .with_max_sample_rate();
+    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let sample_format = supported_config.sample_format();
+    let config = supported_config.into();
+    let stream = match sample_format {
+        SampleFormat::F32 => device.build_output_stream(&config, write_silence::<f32>, err_fn),
+        SampleFormat::I16 => device.build_output_stream(&config, write_silence::<i16>, err_fn),
+        SampleFormat::U16 => device.build_output_stream(&config, write_silence::<u16>, err_fn),
+    }.unwrap();
+    stream.play().unwrap();
 }
