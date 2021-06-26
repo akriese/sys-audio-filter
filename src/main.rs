7b7fb96dad8cc789b34cc3d1db4fb28c1ca86d9a
Emilio Kempkes <emikmp@hotmail.de>
2021-06-26 17:31:59 +0200

    Läuft noch nicht so, wie man will, aber bald

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use psimple::Simple;
 use std::fs;
 use std::io::prelude::*;
 
+use rodio::{Sink, buffer::SamplesBuffer, OutputStream, source::Source};
+
 fn main() {
     // To do: Verallgemeinern
     let spec = Spec {
@@ -12,17 +14,72 @@ fn main() {
         rate: 44100,
     };
     
-    // To do: User gibt Source und Sink an oder wählt aus Liste aus
 
     assert!(spec.is_valid());
 
-    let device = "alsa_output.pci-0000_00_1b.0.analog-stereo.monitor";
+    let is_little_endian = spec.format.is_le(); 
 
-    let s = Simple::new(
+    match is_little_endian {
+        Some(i) => { 
+            if i {
+                println!("little endian");
+            } else {
+                println!("big endian");
+            }
+        }
+        None => println!("No information about endianess!"),
+    }
+
+    // To do: User gibt Source und Sink an oder wählt aus Liste aus
+    let input_device = "test.monitor";
+
+    let s1 = Simple::new(
         None,                // Use the default server
         "FooApp",            // Our application’s name
-        Direction::Recording, // We want a playback stream
-        Some(device),           // Use the default device
+        Direction::Record, // We want a playback stream
+        Some(&input_device),           // Use the default device
+        "Music",             // Description of our stream
+        &spec,               // Our sample format
+        None,                // Use default channel map
+        None                 // Use default buffering attributes
+    ).unwrap();
+
+    // Idee: kleinen Vektor als Buffer für Samples bauen (je zwei u8s zu u16s paaren) und in
+    // Schleife solange der immer wieder gefüllt wird an rodio Sink anhängen
+
+    /* 
+    let mut recording: [u8; 2] = [0; 2];
+
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    while true {
+        let mut vec: Vec<u16> = Vec::new();
+
+
+        // let mut i = 0;
+        // while i < 100000 {
+        //    i += 1;
+            s.read(&mut recording).unwrap();
+            let sample_for_sink = u16::from_ne_bytes(recording);
+            vec.push(sample_for_sink);
+        // }
+
+        let source_for_sink = SamplesBuffer::new(2, 44100, vec);
+
+        sink.append(source_for_sink);
+
+        sink.sleep_until_end();
+    }
+    */ 
+
+    // let output_device = 
+
+    let s2 = Simple::new(
+        None,                // Use the default server
+        "FooApp",            // Our application’s name
+        Direction::Playback, // We want a playback stream
+        None,               // Use the default device
         "Music",             // Description of our stream
         &spec,               // Our sample format
         None,                // Use default channel map
@@ -30,12 +87,16 @@ fn main() {
     ).unwrap();
 
 
-    // Hier Song-Datei öffnen und in Array einlesen
-    // let samples = fs::read("testOutput.raw").unwrap();
-    // println!("Es sind soviele u8s drin: {}", samples.len());
+    while true {
+        let mut buffer: [u8; 10] = [0; 10];
+        s1.read(&mut buffer).unwrap();
 
-    s.write(&samples).unwrap();
-    s.drain().unwrap();
+        s2.write(&buffer).unwrap();
+        s2.drain().unwrap();
+    }
+
+    // s.write(&samples).unwrap();
+    // s.drain().unwrap();
     
     // let mut recording: [u8; 5500000] = [0; 5500000];
     // s.read(&mut recording).unwrap();
