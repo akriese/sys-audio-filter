cdebd4068b2caa5f3b6fb1dd4ecc80eb56c1b101
Anton Kriese <anton.kriese@gmx.de>
2021-07-03 17:03:53 +0200

    move forward function to lib.rs; remove InputStreamWrapper; define FilterBox trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,69 +1,155 @@
 pub mod implementations {
-    //use std::io::{Read, Seek};
-    use std::time::Duration;
-    use rodio::Source;
-
-    pub struct InputStreamWrapper//<R>
-        //where
-        //R: Read + Seek,
-    {
-        pub data: Vec<f32>,//<R>,
-        pub current_offset: usize,
-        pub config: StreamConfig,
-    }
-
-    //impl<R> InputStreamWrapper<R>
-    impl InputStreamWrapper
-    {
-        pub fn new(dat: Vec<f32>, conf: StreamConfig) -> InputStreamWrapper {
-            InputStreamWrapper{data: dat, current_offset: 0, config: conf}
-        }
-    }
-
-    //impl<R> Iterator for InputStreamWrapper<R>
-    impl Iterator for InputStreamWrapper
-        //where
-        //R: Read + Seek,
-    {
-        type Item = f32;
-
-        fn next(&mut self) -> Option<f32> {
-            if self.current_offset >= self.data.len() {
-                return None
-            }
-            else {
-                let sample = self.data[self.current_offset];
-                self.current_offset += 1;
-                return Some(sample)
-            }
-        }
-    }
-
-    //impl<R> Source for InputStreamWrapper<R>
-    impl Source for InputStreamWrapper
-        //where
-        //R: Read + Seek,
-    {
-        fn current_frame_len(&self) -> Option<usize> {
-            Some(self.data.len() - self.current_offset)
-        }
-
-        fn channels(&self) -> u16 {
-            self.config.channels
-        }
-
-        fn sample_rate(&self) -> u32 {
-            return self.config.sample_rate
-        }
-
-        fn total_duration(&self) -> Option<Duration> {
-            None
-        }
-    }
+    extern crate cpal;
+    extern crate anyhow;
+    use cpal::{SampleFormat};
+    use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
+    use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+    use rodio::{Sample, Sink, OutputStream, buffer::SamplesBuffer};
 
     #[derive(Copy, Clone)]
     pub struct StreamConfig {
         pub sample_rate: u32,
         pub channels: u16,
+    }
+
+    pub trait FilterBox {
+        fn init(&mut self) -> Result<(), anyhow::Error>;
+        fn play(&self) -> Result<(), anyhow::Error>;
+        fn set_filter(&self, freq: usize, target_high_pass: bool);
+    }
+
+    pub struct CpalMgr {
+        input_device: cpal::Device,
+        output_device: cpal::Device,
+        //in_cfg: cpal::SupportedStreamConfig,
+        //out_cfg: cpal::SupportedStreamConfig,
+        //sink: Arc<rodio::Sink>,
+        //stream_handle: rodio::OutputStreamHandle,
+        //low_pass:
+        //high_pass:
+        is_finished: Arc<AtomicBool>,
+    }
+
+    fn put_to_sink<R>(data: &[R], sink: &Arc<Sink>, channels: u16, sample_rate: u32)
+        where
+            R: Sample + Send + 'static, // idk why, but this only works with the 'static
+        {
+            // create new source from data and stream configuration
+            //let source = InputStreamWrapper::new(data.to_vec(), in_conf);
+
+            // apparently, there already is an alternative for our Wrapper:
+            let source = SamplesBuffer::new(channels, sample_rate, data);
+            //Source::low_pass(source, 1000);
+            sink.append(source);
+
+            //This is an alternative, which has worse quality than using the sink
+            //stream_handle.play_raw(source).expect("Error while playbacking!");
+        }
+
+    impl CpalMgr {
+        pub fn new() -> Result<CpalMgr, anyhow::Error>{
+            let host = cpal::default_host();
+            let input_device = host.input_devices()?
+                .find(|x| x.name().map(|y| y == "CABLE Output (VB-Audio Virtual Cable)")
+                      .unwrap_or(false))
+                .expect("Failed to find input device!");
+
+            let output_device = host.output_devices()?
+                .find(|x| x.name().map(|y| y == "Lautsprecher (Realtek(R) Audio)")
+                      .unwrap_or(false))
+                .expect("Failed to find input device!");
+
+
+            let is_finished = Arc::new(AtomicBool::new(false));
+
+
+            Result::Ok(CpalMgr{ input_device, output_device, is_finished })
+        }
+    }
+
+    impl FilterBox for CpalMgr {
+        fn init(&mut self) -> Result<(), anyhow::Error> {
+            let host = cpal::default_host();
+            self.input_device = host.input_devices()?
+                .find(|x| x.name().map(|y| y == "CABLE Output (VB-Audio Virtual Cable)")
+                      .unwrap_or(false))
+                .expect("Failed to find input device!");
+
+            self.output_device = host.output_devices()?
+                .find(|x| x.name().map(|y| y == "Lautsprecher (Realtek(R) Audio)")
+                      .unwrap_or(false))
+                .expect("Failed to find input device!");
+
+            self.is_finished = Arc::new(AtomicBool::new(false));
+
+            Result::Ok(())
+        }
+
+        fn play(&self) -> Result<(), anyhow::Error> {
+            let (_stream, stream_handle) = OutputStream::try_from_device(&self.output_device)?;
+            let in_cfg = self.input_device.default_input_config()?;
+            println!("Default input config: {:?}", in_cfg);
+
+            let out_cfg = self.output_device.default_output_config()?;
+            println!("Default output config: {:?}", out_cfg);
+
+            let sink = Arc::new(Sink::try_new(&stream_handle).expect("couldnt build sink"));
+            //let sink_clone = self.sink.clone();
+            let sink_clone = sink.clone();
+
+            let stream_conf = StreamConfig{
+                sample_rate: in_cfg.sample_rate().0,
+                channels: in_cfg.channels()
+            };
+
+            let err_fn = |err| eprintln!("an error occurred on either audio stream: {}", err);
+
+
+            let in_stream =
+                match in_cfg.sample_format() {
+                    SampleFormat::F32 =>
+                        self.input_device.build_input_stream(&in_cfg.clone().into(),
+                        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                            let source = SamplesBuffer::new(stream_conf.channels, stream_conf.sample_rate, data);
+                            sink_clone.append(source);
+                            //put_to_sink::<f32>(data, &sink_clone, in_conf.channels, in_conf.sample_rate);
+                        }, err_fn),
+                    SampleFormat::I16 =>
+                        self.input_device.build_input_stream(&in_cfg.clone().into(),
+                        move |data: &[i16], _: &cpal::InputCallbackInfo| {
+                            put_to_sink::<i16>(data, &sink_clone, stream_conf.channels, stream_conf.sample_rate);
+                        }, err_fn),
+                    SampleFormat::U16 =>
+                        self.input_device.build_input_stream(&in_cfg.clone().into(),
+                        move |data: &[u16], _: &cpal::InputCallbackInfo| {
+                            put_to_sink::<u16>(data, &sink_clone, stream_conf.channels, stream_conf.sample_rate);
+                        }, err_fn),
+                }
+            .unwrap();
+
+            // use Ctrl+C handler to interrupt infinite sleeping loop
+            let is_finished_cln = self.is_finished.clone();
+            ctrlc::set_handler(move || {
+                is_finished_cln.store(true, Ordering::Relaxed);
+                println!("Keyboard Interrupt received!");
+            }).expect("Error setting Ctrl+C handler");
+
+            // start playback
+            in_stream.play()?;
+            //self.sink.sleep_until_end();
+            sink.sleep_until_end();
+            loop {
+                if self.is_finished.load(Ordering::Relaxed) {
+                    break;
+                }
+                std::thread::sleep(std::time::Duration::from_millis(200));
+            }
+
+            Result::Ok(())
+        }
+
+        fn set_filter(&self, freq: usize, target_high_pass: bool) {
+
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 extern crate cpal;
 extern crate anyhow;
-use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
-use cpal::{SampleFormat};
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use rodio::{Source, Sample, Sink, OutputStream, buffer::SamplesBuffer};
-use ctrlc;
-pub use sys_audio_filter::implementations::{InputStreamWrapper, StreamConfig};
+use cpal::traits::{DeviceTrait, HostTrait};
+pub use sys_audio_filter::implementations::{FilterBox, CpalMgr};
 
 fn enum_devices() -> Result<(), anyhow::Error> {
     let available_hosts = cpal::available_hosts();
@@ -25,106 +21,11 @@ fn enum_devices() -> Result<(), anyhow::Error> {
     Result::Ok(())
 }
 
-fn forward_input_to_output() -> Result<(), anyhow::Error> {
-    let host = cpal::default_host();
-    let in_device = host.input_devices()?
-        .find(|x| x.name().map(|y| y == "CABLE Output (VB-Audio Virtual Cable)")
-        .unwrap_or(false))
-        .expect("Failed to find input device!");
-
-    let out_device = host.output_devices()?
-        .find(|x| x.name().map(|y| y == "Lautsprecher (Realtek(R) Audio)")
-        .unwrap_or(false))
-        .expect("Failed to find input device!");
-
-    let (_stream, stream_handle) = OutputStream::try_from_device(&out_device)?;
-    let in_config = in_device.default_input_config()?;
-    println!("Default input config: {:?}", in_config);
-
-    let out_config = out_device.default_output_config()?;
-    println!("Default output config: {:?}", out_config);
-
-    let err_fn = |err| eprintln!("an error occurred on either audio stream: {}", err);
-
-    let in_conf = StreamConfig{
-        sample_rate: in_config.sample_rate().0,
-        channels: in_config.channels()
-    };
-    let sink = Arc::new(Sink::try_new(&stream_handle).expect("couldnt build sink"));
-    let sink_clone = sink.clone();
-
-    let in_stream =
-        match in_config.sample_format() {
-            SampleFormat::F32 =>
-                in_device.build_input_stream(&in_config.into(),
-                move |data: &[f32], _: &cpal::InputCallbackInfo| {
-                    let source = SamplesBuffer::new(in_conf.channels, in_conf.sample_rate, data);
-                    //let source = Source::low_pass(source, 3000);
-                    sink_clone.append(source);
-                    //put_to_sink::<f32>(data, &sink_clone, in_conf.channels, in_conf.sample_rate);
-                }, err_fn),
-            SampleFormat::I16 =>
-                in_device.build_input_stream(&in_config.into(),
-                move |data: &[i16], _: &cpal::InputCallbackInfo| {
-                    put_to_sink::<i16>(data, &sink_clone, in_conf.channels, in_conf.sample_rate);
-                }, err_fn),
-            SampleFormat::U16 =>
-                in_device.build_input_stream(&in_config.into(),
-                move |data: &[u16], _: &cpal::InputCallbackInfo| {
-                    put_to_sink::<u16>(data, &sink_clone, in_conf.channels, in_conf.sample_rate);
-                }, err_fn),
-        }
-        .unwrap();
-
-    // use Ctrl+C handler to interrupt infinite sleeping loop
-    let game_over: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
-    let game_over_clone = game_over.clone();
-    ctrlc::set_handler(move || {
-        game_over_clone.store(true, Ordering::Relaxed);
-        println!("Keyboard Interrupt received!");
-    }).expect("Error setting Ctrl+C handler");
-
-    // start playback
-    in_stream.play()?;
-    sink.sleep_until_end();
-
-    let mut initial_vol = 1.0;
-    let mut cntr = 0;
-    // wait in an infinite loop and wait for Keyboard Interrupt
-    loop {
-        cntr += 1;
-        if game_over.load(Ordering::Relaxed) {
-            break;
-        }
-        if cntr % 5 == 0 {
-            initial_vol -= 0.1;
-            sink.set_volume(initial_vol);
-        }
-        std::thread::sleep(std::time::Duration::from_millis(200));
-    }
-
-    // delete stream instance
-    drop(in_stream);
-    Result::Ok(())
-}
-
-fn put_to_sink<R>(data: &[R], sink: &Arc<Sink>, channels: u16, sample_rate: u32)
-where
-    R: Sample + Send + 'static, // idk why, but this only works with the 'static
-{
-    // create new source from data and stream configuration
-    //let source = InputStreamWrapper::new(data.to_vec(), in_conf);
-
-    // apparently, there already is an alternative for our Wrapper:
-    let source = SamplesBuffer::new(channels, sample_rate, data);
-    //Source::low_pass(source, 1000);
-    sink.append(source);
-
-    //This is an alternative, which has worse quality than using the sink
-    //stream_handle.play_raw(source).expect("Error while playbacking!");
-}
-
 fn main() {
-    enum_devices().expect("Well, something went wrong apparently...");
-    forward_input_to_output();
+    enum_devices().expect("Error enumerating devices!");
+    let mut filter_box: CpalMgr = CpalMgr::new().unwrap();
+
+    //filter_box.init().expect("Error initiating the box!");
+
+    filter_box.play().expect("Error playing the sound!");
 }
